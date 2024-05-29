5601e9d8b14ecd5fcd7ba80bb259fe8eeadd6c03
Patrick Santos <patrick.sg.santos@gmail.com>
2024-05-29 19:58:11 -0300

    implement the ActionCable to refresh the page with all results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,10 @@ class SearchController < ApplicationController
   def index
     @query = Article.ransack(params[:q])
     @articles = @query.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @articles }
+    end
   end
 end
