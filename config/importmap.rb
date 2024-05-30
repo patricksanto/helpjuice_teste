b81fdad88dcbd9a727206a9bde3efc9586661372
Patrick Santos <patrick.sg.santos@gmail.com>
2024-05-30 14:33:51 -0300

    changes in the importmap to fix a stimulus bug in production

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,10 +9,10 @@
 # pin "@rails/actioncable", to: "actioncable.esm.js"
 
 # config/importmap.rb
-pin "application", to: "application.js"
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/channels", under: "channels"
 pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
